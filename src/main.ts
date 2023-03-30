fe52189fb2aacfa9206f0ad4f5da3e605250a3e2
Napat Jirarattanakunchai <69129078+Napatji@users.noreply.github.com>
2023-03-31 06:43:20 +0700

    Configured Vue Router

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import {createRouter,createWebHashHistory} from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Home from './components/Home.vue'
 import Nakhonsi from './components/Nakhonsi.vue'
 import Nakhonsawan from './components/Nakhonsawan.vue'
 import Nonthaburi from './components/Nonthaburi.vue'
@@ -9,6 +10,7 @@ import Narathiwat from './components/Nara.vue'
 import Uthaithani from './components/Uthaithani.vue'
 
 const routes = [
+  { path: '/', component: Home},
   { path: '/NakhonSiThammarat', component: Nakhonsi},
   { path: '/NakhonSawan', component: Nakhonsawan},
   { path: '/Nonthaburi', component: Nonthaburi},
