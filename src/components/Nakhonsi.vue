166068f68a5be19855956eb13038a7cf65ff9441
Napat Jirarattanakunchai <69129078+Napatji@users.noreply.github.com>
2023-04-02 23:14:12 +0700

    add TempleList.vue

--- src/components/Nakhonsi.vue
+++ src/components/Nakhonsi.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
-
+import TempleList from './TempleList.vue';
 </script>
 
 <template>
-
-  <h1>นครศรีธรรมราช</h1>
-
+  <div>
+    <TempleList/>
+  </div>
 </template>
 
 <style scoped>
-
+*{
+  background-color: #B3CEE1;
+  margin: 0%;
+}
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 5%;
+}
 </style>
---
+++ src/components/TempleList.vue
@@ -0,0 +1,79 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      loading: false,
+      error: null,
+      //ตัวแปรข้อมูล
+      post: {fact:'',length:''}
+    }
+  },
+  methods: {
+    getData() {
+      fetch("https://catfact.ninja/fact")
+      .then(response => response.json())
+      .then(data => this.post = data)  
+    }
+  },
+  mounted() {
+    this.getData()
+  }
+}
+
+</script>
+
+<template>
+  <div class="view">
+    <div class="list-box">
+      <h2>{{ post.fact }}</h2>
+    </div>
+    <div class="button-box">
+      <button @click="getData()">Export CSV</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.view {
+  display: flex;
+  flex-direction: column;
+  padding: 0%;
+}
+
+h2 {
+  margin: 0%;
+}
+
+.list-box {
+  width: 800px;
+  height: 400px;
+  padding:4rem;
+  background-color: white;
+}
+
+.button-box {
+  width: 100dvw;
+  height: fit-content;
+  padding-top: 8rem;
+  padding-right: 8rem;
+  display: flex;
+  flex-direction: row-reverse;
+}
+
+button {
+  width: 240px;
+  height: 60px;
+  font-size: larger;
+  color: white;
+  background-color: #03204C;
+  border-radius: 8px;
+  transition: 1ms;
+}
+
+button:hover {
+  color: #03204C;
+  background-color: white;
+}
+
+</style>
